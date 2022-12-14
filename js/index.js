db9b961f72600237ecc1248a6959dd8375c13cfd
pdiazq <pdiazq@hotmail.com>
2022-12-14 10:27:58 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ var parcoords = d3.parcoords()("#example")
   .color(color)
   .alpha(0.4);
 // load csv file and create the chart
-d3.csv('analysis10.csv', function(data) {
+d3.csv('analysis16.csv', function(data) {
   parcoords
     .data(data)
     .render()
